7fe0e1a8f64dd3c873d5d2f2e3bbbfff258991ca
BERTHAU Julien <julien.berthau@orange.com>
2017-01-11 14:49:18 +0100

    clean code

--- OpenClassRoomCourses/NodeJScourses/nodeAppTest/app.js
+++ OpenClassRoomCourses/NodeJScourses/nodeAppTest/app.js
@@ -7,22 +7,9 @@ var app = express();
 app.use(morgan('combined')) // Active le middleware de logging
     .use(express.static(__dirname + '/public')) // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
     .use(favicon(__dirname + '/public/favicon.ico')) // Active la favicon indiquée
-    .use(function(req, res){ // Répond enfin
+    .use(function (req, res) { // Répond enfin
         res.send('Hello');
     });
-
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-app.get('/etage/:etagenum/chambre', function (req, res) {
-    res.render('chambre.ejs', {etage: req.params.etagenum});
-});
-
-app.get('/compter/:nombre', function (req, res) {
-    var noms = ['Robert', 'Jacques', 'David'];
-    res.render('compteur.ejs', {compteur: req.params.nombre, noms: noms});
-});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
