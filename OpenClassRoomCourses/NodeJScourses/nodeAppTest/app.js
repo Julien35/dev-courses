65ab56b7da1bdd7b535fc30187353139b8807e5f
BERTHAU Julien <julien.berthau@orange.com>
2017-01-11 14:47:02 +0100

    First app with Express

--- OpenClassRoomCourses/NodeJScourses/nodeAppTest/app.js
+++ OpenClassRoomCourses/NodeJScourses/nodeAppTest/app.js
@@ -1,13 +1,27 @@
-
 var express = require('express');
+var morgan = require('morgan'); // Charge le middleware de logging
+var favicon = require('serve-favicon'); // Charge le middleware de favicon
+
 var app = express();
+
+app.use(morgan('combined')) // Active le middleware de logging
+    .use(express.static(__dirname + '/public')) // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
+    .use(favicon(__dirname + '/public/favicon.ico')) // Active la favicon indiquée
+    .use(function(req, res){ // Répond enfin
+        res.send('Hello');
+    });
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.get('/etage/:etagenum/chambre', function(req, res) {
+app.get('/etage/:etagenum/chambre', function (req, res) {
     res.render('chambre.ejs', {etage: req.params.etagenum});
+});
+
+app.get('/compter/:nombre', function (req, res) {
+    var noms = ['Robert', 'Jacques', 'David'];
+    res.render('compteur.ejs', {compteur: req.params.nombre, noms: noms});
 });
 
 app.listen(3000, function () {
