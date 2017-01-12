8ab8e741306ca1d92abe0d92e9b72ce2c87adff2
BERTHAU Julien <julien.berthau@orange.com>
2017-01-12 10:54:17 +0100

    Mise à jour partiel d'une ressource

--- zeste-de-savoir-courses/rest_api_symfony/src/AppBundle/Controller/PlaceController.php
+++ zeste-de-savoir-courses/rest_api_symfony/src/AppBundle/Controller/PlaceController.php
@@ -39,7 +39,7 @@ class PlaceController extends Controller
         /* @var $place Place */
 
         if (empty($place)) {
-            return new JsonResponse(['message' => 'Place not found'], Response::HTTP_NOT_FOUND);
+            return \FOS\RestBundle\View\View::create(['message' => 'Place not found'], Response::HTTP_NOT_FOUND);
         }
 
         return $place;
@@ -89,24 +89,38 @@ class PlaceController extends Controller
      */
     public function updatePlaceAction(Request $request)
     {
+        return $this->updatePlace($request, true);
+    }
+
+    /**
+     * @Rest\View()
+     * @Rest\Patch("/places/{id}")
+     */
+    public function patchPlaceAction(Request $request)
+    {
+        return $this->updatePlace($request, false);
+    }
+
+    private function updatePlace(Request $request, $clearMissing)
+    {
         $place = $this->get('doctrine.orm.entity_manager')
             ->getRepository('AppBundle:Place')
             ->find($request->get('id')); // L'identifiant en tant que paramètre n'est plus nécessaire
         /* @var $place Place */
 
         if (empty($place)) {
-            return new JsonResponse(['message' => 'Place not found'], Response::HTTP_NOT_FOUND);
+            return \FOS\RestBundle\View\View::create(['message' => 'Place not found'], Response::HTTP_NOT_FOUND);
         }
 
         $form = $this->createForm(PlaceType::class, $place);
 
-        $form->submit($request->request->all());
+        // Le paramètre false dit à Symfony de garder les valeurs dans notre
+        // entité si l'utilisateur n'en fournit pas une dans sa requête
+        $form->submit($request->request->all(), $clearMissing);
 
         if ($form->isValid()) {
             $em = $this->get('doctrine.orm.entity_manager');
-            // l'entité vient de la base, donc le merge n'est pas nécessaire.
-            // il est utilisé juste par soucis de clarté
-            $em->merge($place);
+            $em->persist($place);
             $em->flush();
             return $place;
         } else {
@@ -114,3 +128,4 @@ class PlaceController extends Controller
         }
     }
 }
+
--- zeste-de-savoir-courses/rest_api_symfony/src/AppBundle/Controller/UserController.php
+++ zeste-de-savoir-courses/rest_api_symfony/src/AppBundle/Controller/UserController.php
@@ -40,7 +40,7 @@ class UserController extends Controller
         /* @var $user User */
 
         if (empty($user)) {
-            return new JsonResponse(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
+            return \FOS\RestBundle\View\View::create(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
         }
 
         return $user;
@@ -90,29 +90,42 @@ class UserController extends Controller
      */
     public function updateUserAction(Request $request)
     {
+        return $this->updateUser($request, true);
+    }
+
+    /**
+     * @Rest\View()
+     * @Rest\Patch("/users/{id}")
+     */
+    public function patchUserAction(Request $request)
+    {
+        return $this->updateUser($request, false);
+    }
+
+    private function updateUser(Request $request, $clearMissing)
+    {
         $user = $this->get('doctrine.orm.entity_manager')
             ->getRepository('AppBundle:User')
             ->find($request->get('id')); // L'identifiant en tant que paramètre n'est plus nécessaire
         /* @var $user User */
 
         if (empty($user)) {
-            return new JsonResponse(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
+            return \FOS\RestBundle\View\View::create(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
         }
 
         $form = $this->createForm(UserType::class, $user);
 
-        $form->submit($request->request->all());
+        $form->submit($request->request->all(), $clearMissing);
 
         if ($form->isValid()) {
             $em = $this->get('doctrine.orm.entity_manager');
-            // l'entité vient de la base, donc le merge n'est pas nécessaire.
-            // il est utilisé juste par soucis de clarté
-            $em->merge($user);
+            $em->persist($user);
             $em->flush();
             return $user;
         } else {
             return $form;
         }
     }
+
 }
 
