26d9a14b4af61578b3e59fb59f68549ebf23e06e
BERTHAU Julien <julien.berthau@orange.com>
2017-01-13 16:58:54 +0100

    nodejs todolist : clean code

--- OpenClassRoomCourses/NodeJScourses/tp-todolist/app.js
+++ OpenClassRoomCourses/NodeJScourses/tp-todolist/app.js
@@ -10,7 +10,7 @@ app.use(logger('dev'));
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -18,13 +18,13 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 
 // index page
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 
     var todolist = [
-        { id: 1 ,task: 'Faire les courses'},
-        { id: 2 ,task: 'Nourrir le chat'},
-        { id: 3 ,task: 'Arroser les plantes'},
-        { id: 4 ,task: 'Lire la suite du tuto sur Node.js'}
+        {id: 1, task: 'Faire les courses'},
+        {id: 2, task: 'Nourrir le chat'},
+        {id: 3, task: 'Arroser les plantes'},
+        {id: 4, task: 'Lire la suite du tuto sur Node.js'}
     ]
     res.render('pages/index', {
         todolist: todolist
@@ -32,7 +32,7 @@ app.get('/', function(req, res) {
 });
 
 // about page
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
     res.render('pages/about');
 });
 
